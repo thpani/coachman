2dd032d03f8f19c5115db5149c1672e402f50ca1
Thomas Pani <thomas.pani@gmail.com>
2017-10-13 13:44:30 +0200

    Improve lexer/parser error reporting

--- src/main.ml
+++ src/main.ml
@@ -2,35 +2,28 @@ let fn_prog = Sys.argv.(1)
 let fn_heap = Sys.argv.(2)
 let fn_summary = Sys.argv.(3)
 
-let print_position lexbuf =
+let position_string lexbuf =
   let pos = lexbuf.Lexing.lex_curr_p in
-  Printf.printf "%d:%d" pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1)
+  Printf.sprintf "%d:%d" pos.Lexing.pos_lnum (pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1)
 
 let () =
-  let parse_heap =
-    let input = open_in fn_heap in
-    let lexbuf = Lexing.from_channel input in
-    let heap = Parser_heap.heap Lexer_heap.token lexbuf in
-    let s = Heap.structure_from_heap heap in
-    close_in input ; s
-  in
-  let parse_program path =
+  let parse path p l =
     let input = open_in path in
     let lexbuf = Lexing.from_channel input in
     try
-      let ast = Parser.program Lexer.token lexbuf in
-      let ast_precomp = Ast.precompile_seq ast in
-      close_in input ;
-      Some ast_precomp
+      let parsed = p l lexbuf in
+      close_in input ; parsed
     with
-    | Lexer.Error msg ->
-        prerr_endline ("[Lexer error] " ^ path ^ msg) ;
-        None
-    | Parsing.Parse_error -> begin
-        prerr_endline ("[Parser error] " ^ path) ;
-        print_position lexbuf ;
-        None
-      end
+    | Lexer.Error msg -> Printf.eprintf "[Lexer error] %s @  %s: %s\n" path (position_string lexbuf) msg ; exit (1)
+    | Parsing.Parse_error -> Printf.eprintf "[Parser error] %s @ %s\n" path (position_string lexbuf) ; exit (1)
+  in
+  let parse_heap =
+    let parsed = parse fn_heap Parser_heap.heap Lexer_heap.token in
+    Heap.structure_from_heap parsed
+  in
+  let parse_program path =
+    let ast = parse fn_prog Parser.program Lexer.token in
+    Ast.precompile_seq ast
   in
   let print_stats g =
     let components = (Heap.SCC.scc_list g) in
@@ -41,16 +34,13 @@ let () =
   let init_heap = parse_heap in
   let ast = parse_program fn_prog in
   let ast_summ = parse_program fn_summary in
-  match ast, ast_summ with
-  | Some ast, Some ast_summ ->
-    let cfg = gen_cfg ast in
-    let cfg = Cfg.add_summaries cfg (List.hd ast_summ) in
-    let cfg_dot = (Filename.basename Sys.argv.(1)) ^ ".dot" in
-    let bicfg = Heap.convert init_heap cfg in
-    let bicfg_dot = (Filename.basename Sys.argv.(1)) ^ ".bi.dot" in
-    Cfg.Dot.write_dot cfg cfg_dot ;
-    Heap.Dot.write_dot bicfg bicfg_dot ;
-    Printf.printf "Stats :: " ; print_stats bicfg ;
-    Heap.remove_summary_edges bicfg ;
-    Printf.printf "Stats (ranked) :: " ; print_stats bicfg ;
-  | _ -> ()
+  let cfg = gen_cfg ast in
+  let cfg = Cfg.add_summaries cfg (List.hd ast_summ) in
+  let cfg_dot = (Filename.basename Sys.argv.(1)) ^ ".dot" in
+  let bicfg = Heap.convert init_heap cfg in
+  let bicfg_dot = (Filename.basename Sys.argv.(1)) ^ ".bi.dot" in
+  Cfg.Dot.write_dot cfg cfg_dot ;
+  Heap.Dot.write_dot bicfg bicfg_dot ;
+  Printf.printf "Stats :: " ; print_stats bicfg ;
+  Heap.remove_summary_edges bicfg ;
+  Printf.printf "Stats (ranked) :: " ; print_stats bicfg ;
