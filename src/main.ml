7a5f2dfcb870c0825a03d010588310b335f3664b
Thomas Pani <thomas.pani@gmail.com>
2018-04-10 18:48:32 +0200

    Add command line flag “-f” to analyze individual functions

--- src/main.ml
+++ src/main.ml
@@ -10,25 +10,25 @@ let parse path p l =
     close_in input ; parsed
   with
   | Lexer.Error msg ->
-      Printf.eprintf "[Lexer error] %s @  %s: %s\n" path (position_string lexbuf) msg ;
-      exit (1)
+    Printf.eprintf "[Lexer error] %s @  %s: %s\n" path (position_string lexbuf) msg ;
+    exit (1)
   | Parsing.Parse_error ->
-      Printf.eprintf "[Parser error] %s @ %s\n" path (position_string lexbuf) ;
-      exit (1)
+    Printf.eprintf "[Parser error] %s @ %s\n" path (position_string lexbuf) ;
+    exit (1)
 
 let parse_heap path =
   let parsed = parse path Parser_heap.heaps Lexer_heap.token in
   Ca_seq.structure_from_parsed_heaps parsed
 
-let parse_program path = 
+let parse_program path =
   parse path Parser.program Lexer.token
 
-let pprint_summaries summaries = String.concat " || " (List.map (fun s -> Printf.sprintf "G(%s)" (fst s)) summaries)
+let pprint_summaries = function
+  | [] -> "∅"
+  | summaries ->
+    String.concat " || " (List.map (fun s -> Printf.sprintf "G(%s)" (fst s)) summaries)
 
-let sequentialize (fun_name, ast) summaries = 
-  Debugger.info "bound" "%s || %s\n" fun_name (pprint_summaries summaries) ;
-  Config.dot_basename := Printf.sprintf "%s.%s" !Config.dot_basename fun_name ;
-
+let sequentialize ast summaries =
   let cfg                = Cfg.of_ast ast in
   let cfg_with_summaries = Cfg.add_summaries cfg summaries in
 
@@ -41,15 +41,17 @@ let sequentialize (fun_name, ast) summaries =
 
 let main () =
   let args = ref [] in
+  let func = ref "" in
   let usage = "Usage: codscost [args] <prog> <heap> <summaries>" in
   Arg.parse [
     "-debug", Arg.String (fun s ->
-      Debugger.current_level := Debugger.Debug ;
-      Debugger.current_components := (String.split_on_char ',' s)
-    ), "Enable debugging" ;
+        Debugger.current_level := Debugger.Debug ;
+        Debugger.current_components := (String.split_on_char ',' s)
+      ), "Enable debugging" ;
     "-ai", Arg.Set Config.use_ai, "Enable SCC pruning using abstract interpretation" ;
     "-no_qflia", Arg.Clear Config.qf_lia, "Disable fixing Z3 to QF_LIA" ;
-    "-no_qe", Arg.Clear Config.qe, "Disable quantifier elimination in transition relation computation"
+    "-no_qe", Arg.Clear Config.qe, "Disable quantifier elimination in transition relation computation" ;
+    "-f", Arg.Set_string func , "Specify function to analyze. If not given, a program choosing non-deterministically among all declared functions will be generated."
   ] (fun s -> args := s :: !args) usage
   ;
 
@@ -57,15 +59,26 @@ let main () =
 
   match !args with
   | [ fn_summary ; fn_heap ; fn_prog ] ->
-    let fn_prog_basename = Filename.remove_extension (Filename.basename fn_prog) in
-    let init_heaps = parse_heap fn_heap in
-    let functions = parse_program fn_prog in
-    let summaries = parse_program fn_summary in
-    List.iter (fun fct ->
-      Config.dot_basename := fn_prog_basename ;
-      let cfg_with_summaries, get_color = sequentialize fct summaries in
-      let edge_bound_map = Bound.compute_bounds ~get_edge_color:get_color init_heaps cfg_with_summaries in
-      Bound.print_edge_bound_map edge_bound_map ;
-      Bound.write_bound_dot edge_bound_map get_color cfg_with_summaries
-    ) functions
+    begin
+      let fn_prog_basename = Filename.remove_extension (Filename.basename fn_prog) in
+      let init_heaps = parse_heap fn_heap in
+      let functions = parse_program fn_prog in
+      let summaries = parse_program fn_summary in
+      let fun_name, fun_ast = match !func with
+        | "" -> (* build most general client *)
+          "(" ^ (String.concat " [] " (List.map fst functions)) ^ ")",
+          Ast.build_nondet_switch functions
+        | fun_name ->
+          fun_name, List.assoc fun_name functions
+      in
+          Debugger.info "bound" "%s || %s\n" fun_name (pprint_summaries summaries) ;
+          Config.dot_basename := fn_prog_basename ;
+          let cfg_with_summaries, get_color = sequentialize fun_ast summaries in
+          let edge_bound_map = Bound.compute_bounds ~get_edge_color:get_color init_heaps cfg_with_summaries in
+          Debugger.info "bound" "Result of R/G transition bound analysis:\n" ;
+          Bound.print_edge_bound_map edge_bound_map ;
+          Bound.write_bound_dot edge_bound_map get_color cfg_with_summaries
+        ;
+        Debugger.info "time" "Time taken: %.0fs\n" (Sys.time ())
+    end
   | _ -> prerr_endline usage ; exit 1
