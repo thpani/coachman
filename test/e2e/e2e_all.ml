7514b9e05a14469428a66185d9d79a06c2aa7e92
Thomas Pani <thomas.pani@gmail.com>
2019-04-03 14:46:40 +0200

    Add test-and-set (TAS) spinlock from AoMP

--- test/e2e/e2e_all.ml
+++ test/e2e/e2e_all.ml
@@ -2,7 +2,7 @@ open OUnit2
 
 let suite =
   "e2e tests" >::: [ 
-    E2e_atomic_ref.suite ;
+    E2e_atomic_ref.suite ; E2e_spinlock.suite ;
     E2e_treiber.suite; E2e_ms.suite; E2e_dglm.suite
   ]
 
---
+++ test/e2e/e2e_spinlock.ml
@@ -0,0 +1,17 @@
+open OUnit2
+
+open E2e
+
+let suite_tas = "TAS" >::: [
+  "(lock [] unlock) || G(Lock) || G(Unlock)" >:: test "spinlock"
+    "tas.tiny" "spinlock.heap" "spinlock/spinlock.summaries" "" [
+      0, Scfg.effect_ID, 3, Complexity.Const 1 ;
+      0, Scfg.effect_ID, 7, Complexity.Const 1 ;
+      3, Scfg.effect_ID, 3, Complexity.Unbounded ;
+      3, Scfg.E "Lock", 11, Complexity.Const 1 ;
+      7, Scfg.effect_ID, 11, Complexity.Const 0 ;
+      7, Scfg.E "Release", 11, Complexity.Const 1 ;
+    ] ;
+]
+
+let suite = "Spinlock" >::: [ suite_tas ]
