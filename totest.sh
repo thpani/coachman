e518b5ecda7fb365c637a39feb04b307f2be02d9
Thomas Pani <thomas.pani@gmail.com>
2019-04-03 21:09:14 +0200

    Add script to convert output to unit test

---
+++ totest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -d '' awkScript <<'EOF'
+($5 == "Effect") {
+  print  \
+    $2 ", " \
+    ($6 == "ID" ? "Scfg.effect_ID" : ("Scfg.E \\"" $6 "\\"")) ", " \
+    $4 ", " \
+    "Complexity." \
+    ($8 == "N" ? "Linear \"N\"" : ($8 == "∞" ? "Unbounded" : "Const " $8)) " ;"
+}
+EOF
+
+grep -- '->' | \
+  gsed 's!(\|)\|:! !g' | \
+  awk "$awkScript"
