45c1159ee9fe2557204204663f5cb6691f1afdb1
Thomas Pani <thomas.pani@gmail.com>
2019-04-29 18:09:23 +0200

    Add Treiber push [] pop test case

--- test/e2e/e2e_treiber.ml
+++ test/e2e/e2e_treiber.ml
@@ -38,6 +38,22 @@ let suite = "Treiber" >::: [
         3, Scfg.effect_ID, 1, Complexity.Const 1 ;
         7, Scfg.E "pop"  , 1, Complexity.Const 1 ;
       ] ;
+    "(push [] pop) || G(push) || G(pop)" >:: test "treiber"
+      "treiber.tiny" "treiber.heap" "treiber/treiber.summaries" "" [
+        0, Scfg.effect_ID, 1, Complexity.Const 1 ;
+        0, Scfg.effect_ID, 12, Complexity.Const 1 ;
+        1, Scfg.effect_ID, 4, Complexity.Const 1 ;
+        4, Scfg.effect_ID, 5, Complexity.Linear "N" ;
+        5, Scfg.effect_ID, 6, Complexity.Linear "N" ;
+        6, Scfg.effect_ID, 4, Complexity.Linear "N" ;
+        6, Scfg.E "push", 21, Complexity.Const 1 ;
+        12, Scfg.effect_ID, 13, Complexity.Linear "N" ;
+        13, Scfg.effect_ID, 16, Complexity.Linear "N" ;
+        16, Scfg.effect_ID, 17, Complexity.Linear "N" ;
+        17, Scfg.effect_ID, 12, Complexity.Linear "N" ;
+        13, Scfg.effect_ID, 21, Complexity.Const 1 ;
+        17, Scfg.E "pop", 21, Complexity.Const 1 ;
+    ] ;
   ] ; "partial" >::: [
         "push (empty)" >:: test "treiber"
       "treiber_partial.tiny" "treiber.heap" "empty.summaries" "push" [
