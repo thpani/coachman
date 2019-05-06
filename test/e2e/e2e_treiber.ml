b9c936654a574bfed046496e34b6aab8920b146d
Thomas Pani <thomas.pani@gmail.com>
2019-05-06 13:10:04 +0200

    Add DCAS stack e2e test
    Adapted from V. Vafeiadis. "Shape-value abstraction for verifying linearizability". In VMCAI 2009.

--- test/e2e/e2e_treiber.ml
+++ test/e2e/e2e_treiber.ml
@@ -88,5 +88,39 @@ let suite = "Treiber" >::: [
       17, Scfg.effect_ID, 12, Complexity.Linear "N" ;
       17, Scfg.E "pop", 21, Complexity.Const 1 ;
     ]
+  ] ; "dcas" >::: [
+        "push (empty)" >:: test "treiber"
+      "dcas_stack.tiny" "treiber.heap" "empty.summaries" "push" [
+        0, Scfg.effect_ID, 3, Complexity.Const 1 ;
+        3, Scfg.effect_ID, 4, Complexity.Const 1 ;
+        4, Scfg.effect_ID, 5, Complexity.Const 1 ;
+        5, Scfg.effect_ID, 3, Complexity.Const 0 ;
+        5, Scfg.E "push" , 2, Complexity.Const 1 ;
+    ] ;
+  "pop (empty)" >:: test "treiber"
+    "dcas_stack.tiny" "treiber.heap" "empty.summaries" "pop" [
+      0, Scfg.effect_ID, 3, Complexity.Const 1 ;
+      3, Scfg.effect_ID, 6, Complexity.Const 1 ;
+      6, Scfg.effect_ID, 7, Complexity.Const 1 ;
+      7, Scfg.effect_ID, 0, Complexity.Const 0 ;
+      3, Scfg.effect_ID, (-1), Complexity.Const 1 ;
+      7, Scfg.E "pop"  , 1, Complexity.Const 1 ;
+    ] ;
+  "(push [] pop) || G(push) || G(pop)" >:: test "treiber"
+    "dcas_stack.tiny" "treiber.heap" "treiber/treiber.summaries" "" [
+        0, Scfg.effect_ID, 1, Complexity.Const 1 ;
+        0, Scfg.effect_ID, 12, Complexity.Const 1 ;
+        1, Scfg.effect_ID, 4, Complexity.Const 1 ;
+        4, Scfg.effect_ID, 5, Complexity.Linear "N" ;
+        5, Scfg.effect_ID, 6, Complexity.Linear "N" ;
+        6, Scfg.effect_ID, 4, Complexity.Linear "N" ;
+        6, Scfg.E "push", 21, Complexity.Const 1 ;
+        12, Scfg.effect_ID, 13, Complexity.Linear "N" ;
+        13, Scfg.effect_ID, 16, Complexity.Linear "N" ;
+        16, Scfg.effect_ID, 17, Complexity.Linear "N" ;
+        17, Scfg.effect_ID, 12, Complexity.Linear "N" ;
+        13, Scfg.effect_ID, (-1), Complexity.Const 1 ;
+        17, Scfg.E "pop", 21, Complexity.Const 1 ;
+    ]
   ]
 ]
